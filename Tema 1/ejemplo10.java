1fbdfaf163a38ad03b44c48bc1ed03870503c871
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-04 08:59:46 +0200

    ejemplos 10 y 11

---
+++ Tema 1/ejemplo10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ejemplo10 {
+    public static void main(String[] args) {
+        System.out.println("Introduzca un valor");
+        Scanner kdb = new Scanner(System.in);
+        double x = kdb.nextDouble();
+        kdb.close();
+        if (x<0) {
+            System.out.println("Número negativo");
+        }
+        else {
+            System.out.println("Número positivo");
+        }
+    }
+}
