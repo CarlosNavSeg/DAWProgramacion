84973de915a6a28a93f1851e12fcd29e015caf58
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-06 09:34:03 +0200

    Errores de sintaxis y ejemplo 15.

---
+++ Tema 1/ejemplo16.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ejemplo16 {
+    public static void main(String[] args) {
+        Scanner kdb = new Scanner(System.in);
+        System.out.println("Introduzca dos números enteros");
+
+        int x = kdb.nextInt();
+        int y = kdb.nextInt();
+
+        System.out.println("Introduzca el carácter de la operación que quiere realizar");
+        String operación = kdb.next();
+        
+        kdb.close();
+        
+        switch(operación) {
+            case "+" :
+            System.out.println(x + y);
+            break;
+
+            case "-" :
+            System.out.println(x - y);
+            break;
+
+            case "*" :
+            System.out.println(x * y);
+            break;
+            
+            case "/" :
+            System.out.println(x / y);
+            break;
+
+            default:
+            System.out.println("Eso no es un carácter operador");
+            break;
+        }
+
+    }
+}
--- Tema 1/ejemplo9.java
+++ Tema 1/ejemplo9.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class ejemplo9 {
     public static void main(String[] args) {
         System.out.println("Introduzca su edad:");
+
         Scanner kdb = new Scanner(System.in);
         int edad = kdb.nextInt();
         kdb.close();
-        if (edad>18) {
+
+        if (edad>=18) {
             System.out.println("Tienes la edad legal");
         }
         else {
