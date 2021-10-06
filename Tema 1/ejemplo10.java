80e77466de5cfd183a1de667127289c5e2556112
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-06 09:35:05 +0200

    Error commit anterior: también ejemplo 16.

--- Tema 1/ejemplo10.java
+++ Tema 1/ejemplo10.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class ejemplo10 {
     public static void main(String[] args) {
         System.out.println("Introduzca un valor");
+
         Scanner kdb = new Scanner(System.in);
         double x = kdb.nextDouble();
+
         kdb.close();
+        
         if (x<0) {
             System.out.println("Número negativo");
         }
