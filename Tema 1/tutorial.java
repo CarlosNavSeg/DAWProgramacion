5ee0c05267c0c8379799d6cad8d5353c8a332b90
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-11 09:57:59 +0200

    Troubleshooting.

---
+++ Tema 1/tutorial.java
@@ -0,0 +1,13 @@
+class tutorial {
+    public static void main(String[] args) {
+        for (int i = 0; i<10 ; i++){
+            System.out.println("Hola:" + i);
+        }
+        int contador = 0;
+        
+        while (contador < 10) {
+            System.out.println("Hola" + contador);
+            contador++;
+        }
+    } 
+}
