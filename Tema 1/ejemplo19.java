8c14c88b5fb2779ce76277692f9cac2d5fb9ee3b
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-13 23:01:24 +0200

    Ejemplo 19 en la home.

--- Tema 1/ejemplo19.java
+++ Tema 1/ejemplo19.java
@@ -13,31 +13,27 @@ public class ejemplo19 {
       double salariobruto = salario*horas;
 
       if (horas > 35) {
-          salariobruto = salariobruto + 1.5 * horas;
+          salariobruto = salariobruto + 1.5 * (horas -35);
 
           if (salariobruto > 500 && salariobruto <= 900) {
             
            double impuesto1 = salariobruto*0.25;
            double salarioTot = salariobruto - impuesto1;
 
+           System.out.println(impuesto1);
            System.out.println(salarioTot);
-           System.out.println("Impuestos:" + impuesto1);
 
             if (salarioTot>900) {
                 double impuesto2 = salarioTot * 0.45 ;
-                System.out.println(salarioTot - impuesto2);
-                System.out.println("Impuestos:" + (impuesto1 + impuesto2));
+                System.out.println((impuesto1 + impuesto2));
+                System.out.println(salarioTot - impuesto2);              
             }
-            else {
-                System.out.println(salarioTot);
-                System.out.println("Impuestos:" + impuesto1);
-            }
-          }
 
             else {
                 System.out.println(salariobruto);
             }
           }
+        }
 
        else {
             System.out.println(salariobruto);
@@ -45,5 +41,10 @@ public class ejemplo19 {
         
           salariohora.close();
 
-    }
-}
+  
+        }
+      } 
+
+
+        
+        
