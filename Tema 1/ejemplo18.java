76c1102188c29f676e46b20128ffd253b16ec218
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-13 08:28:01 +0200

    Ejemplo 18 optimizado. Correción de formato.

--- Tema 1/ejemplo18.java
+++ Tema 1/ejemplo18.java
@@ -6,18 +6,32 @@ public class ejemplo18 {
         //Método 1
         System.out.println("Introduce la hora (usa dos dígitos)");
         byte hora = reloj.nextByte();
-
-        System.out.println("Introduce los minutos (usa dos dígitos)");
-        byte minutos = reloj.nextByte();
-
-        System.out.println("Introduce los segundos(usa dos dígitos)");
-        byte segundos = reloj.nextByte();
-
-        reloj.close();
         
-        System.out.println(hora + ":" + minutos + ":" + (segundos+1));
-        
+        if (hora >=0 && hora<24) {
+            System.out.println("Introduce los minutos (usa dos digítos)");
+            byte minutos = reloj.nextByte();
+            
+            if (minutos >= 0 && minutos < 60) {
+                    System.out.println("Introduce los segundos (usa dos dígitos)");
+                    byte segundos = reloj.nextByte();
+                    
+                    if (segundos >= 0 && segundos < 60) {
+                    System.out.println(hora + ":" + minutos + ":" + (segundos+1));
+                    }
+                    else {
+                        System.out.println("Formato incorrecto");
+                    }
+                    
+            }
+            else {
+                    System.out.println("Formato incorrecto");
+            }
+        }
 
+        else {
+            System.out.println("Formato incorrecto");
+        }
 
+        reloj.close();        
     }
 }
