2a05f5755da9f520c65740cde0d6f1efd0f14fc4
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-13 08:33:17 +0200

    Ejemplo 17, optimizado.

--- Tema 1/ejemplo17.java
+++ Tema 1/ejemplo17.java
@@ -12,51 +12,24 @@ public class ejemplo17 {
 
         switch (mes) {
             case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
             System.out.println("El mes de Enero tiene 31 días ");
             break;
 
             case 2:
-            System.out.println("El mes de Febrero tiene 28 días");
-            break;
-
-            case 3:
-            System.out.println("El mes de Marzo tiene 31 días");
+            System.out.println("El mes tiene 28 días");
             break;
 
             case 4:
-            System.out.println("El mes de Abril tiene 30 días");
-            break;
-
-            case 5:
-            System.out.println("El mes de Mayo tiene 31 días");
-            break;
-
             case 6:
-            System.out.println("El mes de Junio tiene 30 días");
-            break;
-
-            case 7:
-            System.out.println("El mes de Julio tiene 31 días");
-            break;
-
-            case 8:
-            System.out.println("El mes de Agosto tiene 31 días");
-            break;
-            
             case 9:
-            System.out.println("El mes de Septiembre tiene 30 días");
-            break;
-
-            case 10:
-            System.out.println("El mes de Octubre tiene 31 días");
-            break;
-
             case 11:
-            System.out.println("El mes de Noviembre tiene 30 días");
-            break;
-
-            case 12:
-            System.out.println("El mes de Diciembre tiene 31 días");
+            System.out.println("El mes tiene 30 días");
             break;
 
             default:
