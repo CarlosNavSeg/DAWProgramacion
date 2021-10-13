2e264a5ce932fc8fb77ffcbbb1c4629f3e6477f8
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-13 09:01:23 +0200

    Parón control.

--- Tema 1/ejemplo19.java
+++ Tema 1/ejemplo19.java
@@ -2,9 +2,39 @@ import java.util.Scanner;
 
 public class ejemplo19 {
     public static void main(String[] args) {
-      
-        
+      Scanner salariohora = new Scanner(System.in);
 
+      System.out.println("Introduce el salario por hora");
+      double salario = salariohora.nextDouble();
+
+      System.out.println("Introduce las horas de trabajo realizadas");
+      double horas = salariohora.nextDouble();
+
+      double salariobruto = salario*horas;
+
+      if (horas > 35) {
+          salariobruto = salariobruto + 1.5 * horas;
+
+          if (salariobruto > 500 && salariobruto <= 900) {
+           double salarioneto = (salariobruto + salariobruto) *0.75;
+
+            if (salarioneto>900) {
+                salarioneto = (salarioneto + salarioneto) * 0.55;
+            }
+            else {
+                System.out.println(salarioneto);
+            }
+          }
+
+            else {
+                System.out.println(salariobruto);
+            }
+          }
+
+       else {
+            System.out.println(salariobruto);
+        }
+          salariohora.close();
 
     }
 }
