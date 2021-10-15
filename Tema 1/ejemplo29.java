8d3d2989dbdc2337b720bd740d43ed3980970398
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-15 09:28:31 +0200

    Parón.

---
+++ Tema 1/ejemplo29.java
@@ -0,0 +1,18 @@
+public class ejemplo29 {
+    public static void main(String[] args) {
+
+        for(int i=3; i<100 ; i++ ) {
+            for(int j=2; j<i/2 ; j++) {
+                if(i%j == 0 ) {
+
+                }
+
+                else {
+                    System.out.println(i);
+                }
+            }
+ 
+
+        }
+    }
+}
