6e1e452ff9e4dfa19edf1ab6b388c218be7b68f3
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-15 08:31:32 +0200

    Troubleshoot 27

--- Tema 1/ejemplo27.java
+++ Tema 1/ejemplo27.java
@@ -5,12 +5,14 @@ class ejemplo27{
         Scanner numero = new Scanner(System.in);
 
         System.out.println("Introduzca el numero a factorizar");
-        int n = n.nextInt();
-
+        int n = numero.nextInt();
+         String cadena = Integer.toString(n);
         numero.close();
 
         for (int i=0; i<n ; i++) {
-            System.out.println(i);
+            String cadenai = Integer.toString(i);
+            cadena.concat(cadenai);
+            System.out.println(cadena);
         }
 
     }
