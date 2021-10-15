48b229073233722ee53641d114fab661bd6508db
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-15 10:30:24 +0200

    Esperando al día siguiente.

--- Tema 1/ejemplo29.java
+++ Tema 1/ejemplo29.java
@@ -4,24 +4,6 @@ public class ejemplo29 {
         boolean esPrimo = false;
 
         for(int i=3; i <= 100 ; i++ ) {
-
-            int j = 2;
-            esPrimo = false;
-
-            while (j <= i/2) {
-
-                if (i%j == 0) {
-                    esPrimo = false;
-                }
-
-                else {
-                    esPrimo = true;
-                }
-                
-                j++;
-
-            }
-
-        }
+            
     }
 }
