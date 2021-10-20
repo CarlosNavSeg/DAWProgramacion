aac5c93b126889e8c6bfffb822b05bbc3f8ebc17
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-20 10:38:28 +0200

    Sintaxis del 30.

--- Tema 1/ejemplo30.java
+++ Tema 1/ejemplo30.java
@@ -11,13 +11,13 @@ public class ejemplo30 {
         if (longitud <= 3 && palabra.charAt(0) == palabra.charAt(longitud-1)) {
                 System.out.println("Palabra palíndroma puede continuar");
             }
-            if (longitud <= 5 && longitud > 3 && palabra.charAt(1) == palabra.charAt(longitud-2) && palabra.charAt(0) == palabra.charAt(longitud-1)) {
+            if (longitud <= 5 && longitud > 3 && palabra.charAt(0) == palabra.charAt(longitud-1) && palabra.charAt(1) == palabra.charAt(longitud-2) ) {
                     System.out.println("Palabra palíndroma puede continuar");
                 }
-                if(longitud <=7 && longitud > 5 && palabra.charAt(2) == palabra.charAt(longitud-3) && longitud > 7 && palabra.charAt(1) == palabra.charAt(longitud-2) && palabra.charAt(0) == palabra.charAt(longitud-1)) {
+                if(longitud <=7 && longitud > 5 && palabra.charAt(0) == palabra.charAt(longitud-1) &&  palabra.charAt(1) == palabra.charAt(longitud-2) && palabra.charAt(2) == palabra.charAt(longitud-3)   ) {
                         System.out.println("Palabra políndroma puede continuar");
                     }
-                    if(longitud < 9 && palabra.charAt(3) == palabra.charAt(longitud-4) && longitud > 9 && palabra.charAt(2) == palabra.charAt(longitud-3) && longitud > 7 && palabra.charAt(1) == palabra.charAt(longitud-2) && palabra.charAt(0) == palabra.charAt(longitud-1)) {
+                    if(longitud <= 9 && palabra.charAt(0) == palabra.charAt(longitud-1) && palabra.charAt(1) == palabra.charAt(longitud-2) && palabra.charAt(2) == palabra.charAt(longitud-3) && palabra.charAt(3) == palabra.charAt(longitud-4)) {
                             System.out.println("Palabra políndroma puede continuar");
                         }
                     
