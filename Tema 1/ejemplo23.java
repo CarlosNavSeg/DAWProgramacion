0ccd68bcf99fa1af012cc1395f1a0a495b1dbb98
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-20 09:13:57 +0200

    Cambios en el 23.

--- Tema 1/ejemplo23.java
+++ Tema 1/ejemplo23.java
@@ -5,14 +5,20 @@ public class ejemplo23 {
         Scanner numeros = new Scanner(System.in);
         
         System.out.println("Introduce cualquier número '0' para acabar"); 
-
+        int numerosPositivos = 0;
        double entrada;
 
     do {
         
-        entrada = numeros.nextDouble();  
+        entrada = numeros.nextDouble();
+
+        if (entrada > 0) {
+            numerosPositivos++;
+        }  
         
        } while(entrada != 0);
+
+       System.out.println("Has introducido " + numerosPositivos + " numeros positivos");
 
        numeros.close();
        
--- Tema 1/ejemplo32.java
+++ Tema 1/ejemplo32.java
@@ -4,12 +4,14 @@ public class ejemplo32 {
     public static void main(String[] args) {
         Scanner llenararray = new Scanner(System.in);
         int [] a = new int[10];
-        
+
         for (int i = 0; i<a.length ; i++) {
             a[i] = llenararray.nextInt();
         }
 
         llenararray.close();
+
+        
 
 
     }
