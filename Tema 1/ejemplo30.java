f7d88c21ea22afe809e6eb6c518ad4c5731e52d6
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-20 08:36:50 +0200

    Ejemplo 30 acabado.

---
+++ Tema 1/ejemplo30.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ejemplo30 {
+
+    public static void main(String[] args) {
+        Scanner fraseusuario = new Scanner(System.in);
+        boolean políndroma = true;
+
+        while (políndroma == true) {
+        String palabra = fraseusuario.next();
+        int longitud = palabra.length();
+        if (palabra.charAt(0) != palabra.charAt(longitud-1)) {
+            políndroma = false;
+        }
+
+    }
+    }
+}
