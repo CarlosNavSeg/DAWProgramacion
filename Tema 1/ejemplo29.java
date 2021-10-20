896ab082dcaa518b1f3c7beb8e6bd2178339fdce
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-20 08:21:40 +0200

    Ejemplo 29 acabado.

--- Tema 1/ejemplo29.java
+++ Tema 1/ejemplo29.java
@@ -1,9 +1,19 @@
 public class ejemplo29 {
     public static void main(String[] args) {
         
-        boolean esPrimo = false;
-
         for(int i=3; i <= 100 ; i++ ) {
-            
+            int j = 2;
+            int match = 0;
+            while (j<=i/2) {
+                if (i%j == 0) {
+                    match++;
+                }
+
+                j++;
+            }
+            if (match == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
