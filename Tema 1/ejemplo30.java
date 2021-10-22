42fb238e8c51a4b7ace12ecffbe51f05e2eb9e4d
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-22 08:52:08 +0200

    EJemplo 30 acabado.

--- Tema 1/ejemplo30.java
+++ Tema 1/ejemplo30.java
@@ -3,32 +3,42 @@ public class ejemplo30 {
 
     public static void main(String[] args) {
         Scanner fraseusuario = new Scanner(System.in);
-        boolean políndroma = true;
+        boolean polindroma = false;
 
-        while (políndroma == true) {
-        String palabra = fraseusuario.next();
-        int longitud = palabra.length();
-        if (longitud <= 3 && palabra.charAt(0) == palabra.charAt(longitud-1)) {
-                System.out.println("Palabra palíndroma puede continuar");
-            }
-            if (longitud <= 5 && longitud > 3 && palabra.charAt(0) == palabra.charAt(longitud-1) && palabra.charAt(1) == palabra.charAt(longitud-2) ) {
-                    System.out.println("Palabra palíndroma puede continuar");
+        System.out.println("Introduzca una frase");
+        String frase = fraseusuario.next();
+
+        int longitud = frase.length();
+        int j = longitud-1;
+     
+        int error = 0;
+
+        for(int i = 0; i < longitud/2; i++) {
+            
+                if(frase.charAt(i) == frase.charAt(j)) {
+                    polindroma = true;
+                  
                 }
-                if(longitud <=7 && longitud > 5 && palabra.charAt(0) == palabra.charAt(longitud-1) &&  palabra.charAt(1) == palabra.charAt(longitud-2) && palabra.charAt(2) == palabra.charAt(longitud-3)   ) {
-                        System.out.println("Palabra políndroma puede continuar");
-                    }
-                    if(longitud <= 9 && palabra.charAt(0) == palabra.charAt(longitud-1) && palabra.charAt(1) == palabra.charAt(longitud-2) && palabra.charAt(2) == palabra.charAt(longitud-3) && palabra.charAt(3) == palabra.charAt(longitud-4)) {
-                            System.out.println("Palabra políndroma puede continuar");
-                        }
-                    
-        if (palabra.charAt(0) != palabra.charAt(longitud-1)) {
-            System.out.println("Palabra no políndroma, el programa terminará");
-            políndroma = false;
-        }
-    }
-        fraseusuario.close();
 
-    }
+                else {
+                   error++;
+                } 
+                j--;
+            }
+        
+        if (polindroma == true) {
+            System.out.println("Palabra políndroma");
+        }
+
+        if(error != 0) {
+            System.out.println("Palabra no políndroma");
+        }
+   
+    fraseusuario.close();
 }
+        
+
+}
+
 
 
