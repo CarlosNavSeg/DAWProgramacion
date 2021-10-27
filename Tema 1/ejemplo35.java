8c14a7f08674f9327493f4e21e6e69b9f1f28413
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-27 10:32:49 +0200

    Areacirculo en Mates.

--- Tema 1/ejemplo35.java
+++ Tema 1/ejemplo35.java
@@ -1,15 +1,6 @@
-import java.util.Scanner;
+
 
 public class ejemplo35 {
     public static void main(String[] args) {
-        Scanner profesor = new Scanner (System.in);
-        int [] notas;
-        notas = new int[100];
-        boolean wasTen;
-
-        for (int i = 0; i<notas.length; i++) {
-        }
-
-        }
     }
-
+}
--- Tema2/Mates.java
+++ Tema2/Mates.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class Mates {
     public static double suma (double operando1, double operando2) {
         double resultado = operando1 + operando2;
@@ -10,19 +12,25 @@ public class Mates {
     }
 
     public static double multiplicación(double operando1, double operando2) {
-        double resultado = operando1* operando2;
+        double resultado = operando1 * operando2;
         return resultado;
     }
-    
+
     public static double division(double dividendo, double divisor) {
         double resultado = dividendo/divisor;
         return resultado;
+    }
+
+    public static double areacirculo(int radio) {
+        double area = Math.PI * Math.pow(radio, 2);
+        return area;
     }
     public static void main(String[] args) {
         System.out.println(suma(1, 2));
         System.out.println(resta(2,1));
         System.out.println(multiplicación(2, 1));
         System.out.println(division(4,2));
+        System.out.println("El area del circulo de radio 2 es 12,56..." + areacirculo(2));
 
     }
 }
