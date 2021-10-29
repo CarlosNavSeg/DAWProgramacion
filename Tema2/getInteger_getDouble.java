0197a8387f4007df2e8e8bc6cba0906c4efbb287
FeedEmpanao <carlos.raco@hotmail.com>
2021-10-29 10:03:07 +0200

    getInteger realizado.

---
+++ Tema2/getInteger_getDouble.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class getInteger_getDouble {
+    public static int getInteger() {
+        Scanner token = new Scanner(System.in);
+
+        boolean hayInteger = false;
+
+        int valorenInt = 0;
+        String valor;
+
+        System.out.println("Introduce lo que quieras");
+
+        while (hayInteger == false) {
+            valor = token.next();
+
+            Scanner comprobador = new Scanner(valor);
+            comprobador.hasNextInt();
+
+            if (comprobador.hasNextInt() == true) {
+                hayInteger = true;
+                valorenInt = Integer.parseInt(valor);
+                comprobador.close();
+
+            }
+        }
+
+        token.close();
+
+        return valorenInt;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("El resultado después del bucle es:" + getInteger());
+    }
+
+}
