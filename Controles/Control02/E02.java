0cd794b5332bf9d4121544885944501b7009b686
FeedEmpanao <carlos.raco@hotmail.com>
2021-11-05 09:15:14 +0100

    E02 acabado.

---
+++ Controles/Control02/E02.java
@@ -0,0 +1,17 @@
+package Control02;
+
+import java.util.Scanner;
+
+public class E02 {
+    public static void main(String[] args) {
+        Scanner datosproblema = new Scanner(System.in);
+
+        System.out.print("Ingrese el radio: ");
+        double radio = datosproblema.nextDouble();
+        
+        System.out.print("Perimetro: ");
+        double perimetro = datosproblema.nextDouble();
+
+        System.out.println("Área: " + perimetro/2*radio);
+    }
+}
