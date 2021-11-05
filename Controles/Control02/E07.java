64a199d445a855b7e1f0bd520cfc7dd4a0fcd3c8
FeedEmpanao <carlos.raco@hotmail.com>
2021-11-05 09:52:01 +0100

    E07 acabado.

---
+++ Controles/Control02/E07.java
@@ -0,0 +1,19 @@
+package Control02;
+
+import java.util.Scanner;
+
+public class E07 {
+    public static void main(String[] args) {
+        Scanner reloj = new Scanner(System.in);
+
+        System.out.print("Hora actual: ");
+        int hora = reloj.nextInt();
+
+        System.out.print("Cantidad de horas: ");
+        int horaspasadas = reloj.nextInt();
+
+        System.out.println("En " + horaspasadas + " horas, el reloj marcara las " + (hora + horaspasadas));
+
+        reloj.close();
+    }
+}
