c816bfa245819656a790e3df265e153973f589a5
FeedEmpanao <carlos.raco@hotmail.com>
2021-11-10 09:42:05 +0100

    45 a medias.

---
+++ Tema 1/ejemplo45.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ejemplo45 {
+    public static void main(String[] args) {
+        Scanner entradaValorDecimal = new Scanner(System.in);
+
+        System.out.print("Introduce un valor decimal: ");
+        int valorDecimal = entradaValorDecimal.nextInt();
+
+        
+    }
+}
