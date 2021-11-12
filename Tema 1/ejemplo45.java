fadfafb01c78ff1d871c6a810a6528d246f1ef0b
FeedEmpanao <carlos.raco@hotmail.com>
2021-11-12 10:10:53 +0100

    Ejemplo 45 acabado.

--- Tema 1/ejemplo45.java
+++ Tema 1/ejemplo45.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
-
+import java.lang.Math;
 public class ejemplo45 {
     public static void main(String[] args) {
-        Scanner entradaValorDecimal = new Scanner(System.in);
+        int numero, exp, digito;
+        double binario;
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Introduce un numero entero >= 0: ");                                                
+            numero = sc.nextInt();
 
-        System.out.print("Introduce un valor decimal: ");
-        int valorDecimal = entradaValorDecimal.nextInt();
-        System.out.println("hola");
-
-        entradaValorDecimal.close();
-        
+            exp=0;
+            binario=0;
+        while(numero!=0){
+                digito = numero % 2;           
+                binario = binario + digito * Math.pow(10, exp);                                                   
+                exp++;
+                numero = numero/2;
+        }
+        System.out.printf("Binario: %.0f %n", binario);
+        sc.close();
     }
+
+    
 }
