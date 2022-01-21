256ae33661cb3f4c2ed3a230c78bba07d9852b88
FeedEmpanao <carlos.raco@hotmail.com>
2022-01-21 10:35:20 +0100

    TicketMachine.

---
+++ ObjetosTuto/TicketMachine/TicketMachine2.java
@@ -0,0 +1,105 @@
+package TicketMachine;
+
+public class TicketMachine2 {
+    /**
+     * TicketMachine models a ticket machine that issues
+     * flat-fare tickets.
+     * The price of a ticket is specified via the constructor.
+     * Instances will check to ensure that a user only enters
+     * sensible amounts of money, and will only print a ticket
+     * if enough money has been input.
+     * 
+     * @author David J. Barnes and Michael Kölling
+     * @version 2016.02.29
+     */
+    public class TicketMachine {
+        // The price of a ticket from this machine.
+        private double price;
+        // The amount of money entered by a customer so far.
+        private double balance;
+        // The total amount of money collected by this machine.
+        private double total;
+        // El dinero que falta para pagar el ticket.
+        private double amountToPay = price - balance;
+
+        /**
+         * Create a machine that issues tickets of the given price.
+         */
+        public TicketMachine(int cost) {
+            price = cost;
+            balance = 0;
+            total = 0;
+        }
+
+        /**
+         * @Return The price of a ticket.
+         */
+        public double getPrice() {
+            return price;
+        }
+
+        /**
+         * Return The amount of money already inserted for the
+         * next ticket.
+         */
+        public double getBalance() {
+            return balance;
+        }
+
+        /**
+         * Receive an amount of money from a customer.
+         * Check that the amount is sensible.
+         */
+        public void insertMoney(int amount) {
+            if (amount > 0) {
+                balance = balance + amount;
+            } else {
+                System.out.println("Use a positive amount rather than: " +
+                        amount);
+            }
+        }
+
+        /**
+         * Print a ticket if enough money has been inserted, and
+         * reduce the current balance by the ticket price. Print
+         * an error message if more money is required.
+         */
+        public void printTicket() {
+            if (balance >= price) {
+                // Simulate the printing of a ticket.
+                System.out.println("##################");
+                System.out.println("# The BlueJ Line");
+                System.out.println("# Ticket");
+                System.out.println("# " + price + " cents.");
+                System.out.println("##################");
+                System.out.println();
+
+                // Update the total collected with the price.
+                total = total + price;
+                // Reduce the balance by the price.
+                balance = balance - price;
+            } else {
+                System.out.println("You must insert at least: " +
+                        (amountToPay) + " more cents.");
+
+            }
+        }
+
+        /**
+         * Return the money in the balance.
+         * The balance is cleared.
+         */
+        public double refundBalance() {
+            double amountToRefund;
+            amountToRefund = balance;
+            balance = 0;
+            return amountToRefund;
+        }
+
+        public void emptyMachine() {
+            System.out.println("La máquina tiene " + total + "centimos.");
+            total = 0;
+
+        }
+    }
+}
--- ObjetosTuto/TicketMachine/enunciado.md
+++ ObjetosTuto/TicketMachine/enunciado.md
@@ -26,3 +26,27 @@ cuerpo ha de asignar simplemente el valor 0 al campo total. ¿Necesita este
 método algún tipo de parámetro? Comprueba este método creando una máquina,
 introduciendo algo de dinero, imprimiendo billetes, comprobando el total y luego
 vaciando la máquina. ¿Es el método empty un mutador o un selector?.
+-------------------------------------------------------------o----------------------------------
+
+Programación Orientada a Objetos
+better-ticket-machine
+En este proyecto, realiza las siguientes modificaciones:
+1. Escribe un método llamado emptyMachine, diseñado para simular el vaciado del
+dinero acumulado en la máquina. Este método debe reinicializar el campo total a
+cero, pero también devolver el valor que tuviera almacenado en total antes de la
+reinicialización.
+2. Escribe de nuevo el método printTicket para que declare una variable local,
+amountToPay, que debe indicar el dinero que falta para completar el precio del
+billete. Dicha variable deberá inicializarse para que contenga la diferencia entre
+price y balance. Reescribe la comprobación en la instrucción condicional para que
+se compruebe el valor de amountToPay. Si su valor es menor o igual que cero,
+deberá imprimirse el billete; en caso contrario, hay que imprimir un mensaje de
+error que especifique la cantidad de dinero que falta por introducir. Comprueba
+este nuevo método creando varias máquinas e intentando imprimir el billete
+cuando no se ha introducido la cantidad correcta y cuando sí que se ha
+introducido.
+3. Supongamos que queremos permitir que la máquina imprima billetes con
+descuento (20%) que se aplica cuando los usuarios pulsan un botón en la máquina
+física. Implementa un método llamado setDiscount, que realice esta operación.
+Antes de empezar a hacerlo piensa bien qué campos nuevos necesitas y qué
+métodos deberías cambiar.
