74c38b7db9a234ba22b777c3bc0f6ac686623290
FeedEmpanao <carlos.raco@hotmail.com>
2022-02-09 10:24:31 +0100

    Un arraylist que no valía para nada.

--- ObjetosTuto/MusicOrganizer/MusicOrganizerTest.java
+++ ObjetosTuto/MusicOrganizer/MusicOrganizerTest.java
@@ -1,9 +1,7 @@
 package MusicOrganizer;
-import java.util.ArrayList;
 
 public class MusicOrganizerTest {
     public static void main(String[] args) {
-        ArrayList<String> canciones = new ArrayList<>(3);
         MusicOrganizer musica = new MusicOrganizer();
         musica.addFile("match");
         musica.addFile("filename");
