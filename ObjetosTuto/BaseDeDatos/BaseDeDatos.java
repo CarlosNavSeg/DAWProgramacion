8db4aa8c50273b11bbb20894f4d007524aac7087
FeedEmpanao <carlos.raco@hotmail.com>
2022-03-09 10:17:59 +0100

    Buena esa.

--- ObjetosTuto/BaseDeDatos/BaseDeDatos.java
+++ ObjetosTuto/BaseDeDatos/BaseDeDatos.java
@@ -7,25 +7,39 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class BaseDeDatos  {
+public class BaseDeDatos {
 
     public static ArrayList<Usuario> usuarios = new ArrayList<>();
-    
+
     public static Usuario login(String email, String password) throws IOException {
         String data = "";
         Usuario u1 = null;
-        data = new String(Files.readAllBytes(Path.of("/home/vagrant/DAWProgramacion/ObjetosTuto/BaseDeDatos/registro.txt")));
-        while (!data.isEmpty()) {
-            String nextLine = data.substring(0, data.indexOf("\n"));
-            if(nextLine.contains(email)) {
-                if (nextLine.substring(nextLine.indexOf(" "), nextLine.length()).contains(password)) {
-                return u1 = new Usuario(email, password);
+        data = new String(
+                Files.readAllBytes(Path.of("/home/vagrant/DAWProgramacion/ObjetosTuto/BaseDeDatos/registro.txt")));
+        while(!data.isEmpty()) {
+            String nextLine = "";
+            if(data.indexOf("\n") != -1) {
+            nextLine = data.substring(0, data.indexOf("\n"));
+            }
+            else {
+                nextLine = data;
+                data = "";
+            }
+            if (!nextLine.substring(0, nextLine.indexOf(" ")).equals(email)) {
+                if(data.indexOf("\n") != -1){
+                    data = data.substring(nextLine.length() + 1, data.length());
                 }
                 else {
-
+                    data = "";
                 }
-            } 
-            data = "";          
+            }
+                else{
+                if (nextLine.substring(nextLine.indexOf(" ") +1 , nextLine.length()).equals(password)) {
+                    return u1 = new Usuario(email, password);
+                } else {
+                    return null;
+                }
+            }
         }
         return u1;
     }
@@ -43,75 +57,65 @@ public class BaseDeDatos  {
                 if (comprobador.contains(".")) {
                     emailDuplicado = false;
                     String input = "";
-                    try{
-                    input = new String(Files.readAllBytes(Path.of("/home/vagrant/DAWProgramacion/ObjetosTuto/BaseDeDatos/registro.txt")));
-                    while(!input.isEmpty()) {
-                        String nextLine = input.substring(0, input.indexOf("\n"));
-                        if(nextLine.contains(email)) {
-                            emailDuplicado = true;
-                            input = "";
+                    try {
+                        input = new String(Files.readAllBytes(
+                                Path.of("/home/vagrant/DAWProgramacion/ObjetosTuto/BaseDeDatos/registro.txt")));
+                        while (!input.isEmpty()) {
+                            String nextLine = input.substring(0, input.indexOf("\n"));
+                            if (nextLine.contains(email)) {
+                                emailDuplicado = true;
+                                input = "";
+                            } else {
+                                emailDuplicado = false;
+                                input = "";
+                            }
                         }
-                        else {
-                            emailDuplicado = false;
-                            input = "";
-                        }
-                    }
-                    }
-                    catch (IOException e) {
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    if(!emailDuplicado) {
-                        
+                    if (!emailDuplicado) {
+
                         try {
-                        FileWriter fw = new FileWriter("/home/vagrant/DAWProgramacion/ObjetosTuto/BaseDeDatos/registro.txt", true);
-                        System.out.println("Contraseña: ");
-                        String password = kdb.nextLine();
-                        Usuario u1 = new Usuario(email, password);
-                        usuarios.add(u1);
-                        emailCorrecto = true;
-                        fw.write("\n" + email + " " + password);
-                        fw.close();
-                        }
-                        catch (IOException e) {
+                            FileWriter fw = new FileWriter(
+                                    "/home/vagrant/DAWProgramacion/ObjetosTuto/BaseDeDatos/registro.txt", true);
+                            System.out.println("Contraseña: ");
+                            String password = kdb.nextLine();
+                            Usuario u1 = new Usuario(email, password);
+                            usuarios.add(u1);
+                            emailCorrecto = true;
+                            fw.write("\n" + email + " " + password);
+                            fw.close();
+                        } catch (IOException e) {
                             e.printStackTrace();
                         }
                     }
-                
+
                     else {
                         System.out.println("El email ya está siendo utilizado");
                     }
                 }
-            
+
             } else {
                 System.out.println("Email incorrecto");
             }
         } while (!emailCorrecto);
         kdb.close();
-        }
-        
-        
+    }
 
-    
     public void userRemove(String email) {
-        for (Usuario usuario:
-             usuarios) {
-            if(usuario.getEmail().equals(email)) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getEmail().equals(email)) {
                 usuarios.remove(usuario);
             }
         }
     }
-    public void userRemoveEmailPattern(String dominio)  {
-        for (Usuario usuario:
-             usuarios) {
-            if(usuario.getEmail().substring(usuario.getEmail().indexOf("@")).equals(dominio)) {
+
+    public void userRemoveEmailPattern(String dominio) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getEmail().substring(usuario.getEmail().indexOf("@")).equals(dominio)) {
                 usuarios.remove(usuario);
             }
-            }
-            
         }
+
     }
-
-
-
-
-
+}
--- ObjetosTuto/BaseDeDatos/BaseTest.java
+++ ObjetosTuto/BaseDeDatos/BaseTest.java
@@ -1,8 +1,10 @@
 package BaseDeDatos;
 
+import java.io.IOException;
 
 public class BaseTest {
-    public static void main(String[] args) {
-    BaseDeDatos.create();
+    public static void main(String[] args) throws IOException {
+    Usuario u1 = BaseDeDatos.login("yoquierousartunngle@gmail.com", "chispa");
+    System.out.println(u1.getEmail());
     }
 }
