a0c42553405399afb5956529c5d6da5b5c44a8b6
FeedEmpanao <carlos.raco@hotmail.com>
2022-03-14 11:19:57 +0100

    RoleGame inicio.

---
+++ ObjetosTuto/RoleGame/Character/Stat/CharacterStats.java
@@ -0,0 +1,25 @@
+package Stat;
+
+
+public class CharacterStats {
+    private int value = 0;
+
+    public CharacterStats(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    public void increase() {
+        value++;
+    }
+    public void decrease() {
+        value--;
+    }
+    @Override
+    public String toString() {
+        return "CharacterStats [value=" + value + "]";
+    }
+    
+}
---
+++ ObjetosTuto/RoleGame/Character/test.java
@@ -0,0 +1,10 @@
+import Stat.*;
+public class test {
+    public static void main(String[] args) {
+        CharacterStats s = new CharacterStats(2);
+        s.increase();
+        s.decrease();
+    }
+  
+    
+}
