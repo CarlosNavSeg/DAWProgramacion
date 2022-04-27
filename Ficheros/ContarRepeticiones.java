f53e24ca8324ff105d2c35442a0a02b4410d1f0d
FeedEmpanao <carlos.raco@hotmail.com>
2022-04-27 08:07:03 +0200

    Commit de.

---
+++ Ficheros/ContarRepeticiones.java
@@ -0,0 +1,40 @@
+package Ficheros;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+public class ContarRepeticiones {
+    public static void main(String[] args) {
+        File fIn = new File("/home/vagrant/Desktop/Path_TXTS/1.txt");
+        int repCounter = 0;
+        try {
+        Scanner wordScan = new Scanner(fIn);
+        while(wordScan.hasNextLine()) {
+            String nextLine = wordScan.nextLine();
+            for(int i = 0; i< nextLine.length(); i++) {
+                String word = nextLine.substring(0, nextLine.indexOf(" "));
+
+                for(int j = 0; j<args.length; j++) {
+                    if(args[j].contains(word)) {
+                        repCounter++;
+                    }
+                }
+                
+                nextLine = nextLine.substring(word.length() + 1 , nextLine.length());
+                    if(nextLine.indexOf(" ") != -1) {
+                    word = nextLine.substring(0, nextLine.indexOf(" "));
+                }
+                else {
+                    word = nextLine;
+                }
+            }
+        }
+    }
+    catch(FileNotFoundException e) {
+
+    }
+    }
+}
