b0e698d06c0de727bfa49140b37678fd2dca47b5
FeedEmpanao <carlos.raco@hotmail.com>
2022-06-06 09:48:15 +0200

    Conseguida conexión a la BD.

--- Tema7/conexionTest.java
+++ Tema7/conexionTest.java
@@ -1,11 +1,24 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Connection; 
+import java.sql.DriverManager; 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-class conexionTest {
-    
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Connection conexion = DriverManager.getConnection("jdbc:mysql://192.168.204.156:3306", "phpmyadmin", "carlos"); 
-        java.sql.Statement mysStatement = conexion.createStatement();
-    }   
-}
+public class conexionTest {
+        public static void main(String args[]) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection conn = null;
+                conn = DriverManager.getConnection("jdbc:mysql://10.0.2.4/clientes","carlos", "carlos");
+                String query = "SELECT * FROM `Cliente` ";
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery(query);
+                rs.close();
+                conn.close();
+            }
+            catch(Exception e) {
+                System.out.print("Do not connect to DB - Error:");
+                e.printStackTrace();
+            }
+        }
+    }
